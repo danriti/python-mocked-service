5c69623d77bbe5780d5d68dbc5e85bba08ae3770
Dan Riti <dmriti@gmail.com>
2014-06-26 23:11:18 +0000

    Create a repository mock.

--- mocks/github.py
+++ mocks/github.py
@@ -4,3 +4,18 @@
 Mocks for the Github API library tests.
 
 """
+
+
+from httmock import response, urlmatch
+
+
+NETLOC = r'(.*\.)?api\.github\.com$'
+HEADERS = {'content-type': 'application/json'}
+GET = 'get'
+
+
+@urlmatch(netloc=NETLOC, path='/repos', method=GET)
+def repository(url, request):
+    name = url.path.split('/')[-1]
+    content = {'name': name}
+    return response(200, content, HEADERS, None, 5, request)
