c97eb466131c66cd3daf0b4c5e0014a5a4756bb0
Dan Riti <dmriti@gmail.com>
2014-06-26 22:41:04 +0000

    Add get_repository function.

--- github.py
+++ github.py
@@ -8,3 +8,22 @@ This module contains methods for accessing the Github API v3.
 https://developer.github.com/v3/
 
 """
+
+import requests
+
+
+GITHUB_AUTHORITY = 'api.github.com'
+
+
+def get_repository(owner, repo):
+    """ Get respository information.
+
+    :param owner: The name of the repository owner.
+    :param repo: The name of the repository.
+    :rtype: dict
+
+    """
+    uri = 'https://{0}/repos/{1}/{2}'.format(GITHUB_AUTHORITY, owner, repo)
+    response = requests.get(uri)
+    response.raise_for_status()
+    return response.json()
