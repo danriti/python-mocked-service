5003a893b1c52b662d4618a754e921e857e65f9f
Dan Riti <dmriti@gmail.com>
2014-06-26 22:41:59 +0000

    Add test_get_repository test case.

--- test_github.py
+++ test_github.py
@@ -4,3 +4,25 @@
 Tests for the Github API library.
 
 """
+
+import unittest
+
+import github
+
+
+class TestGithub(unittest.TestCase):
+
+    def test_get_repository(self):
+        owner = 'appneta'
+        repo = 'burndown'
+
+        results = github.get_repository(owner, repo)
+
+        self.assertNotEqual(results, None)
+        self.assertIsInstance(results, dict)
+        self.assertTrue('name' in results)
+        self.assertEqual(results['name'], repo)
+
+
+if __name__ == '__main__':
+    unittest.main()
